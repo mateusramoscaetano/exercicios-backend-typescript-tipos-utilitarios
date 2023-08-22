6653070ea319ccbd7695658457a355dbc5403324
Mateus Caetano <mateus-ramoshd@hotmail.com>
2023-08-22 08:29:49 -0300

    resolvido

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -1,11 +1,17 @@
 type Usuario = {
-    nome: string,
-    email: string,
-    cpf: string,
-    rg: string
-    dataNacimento?: string
-}
+  nome: string;
+  email: string;
+  cpf: string;
+  rg: string;
+  dataNacimento?: string;
+};
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
-    return info;
-}
+  return info;
+};
+
+type UsuarioAtualizacao = Omit<Usuario, "rg">;
+
+const atualizarUsuario = (info: UsuarioAtualizacao): Partial<Usuario> => {
+  return info;
+};
--- 03/scripts.ts
+++ 03/scripts.ts
@@ -1,15 +1,15 @@
 type Conn = {
-    username: string,
-    password: string,
-    host: string,
-    port: string,
-    dbname: string
-}
+  username: string;
+  password: string;
+  host: string;
+  port: string;
+  dbname: string;
+};
 
-const conexao = (dados: Conn) => {
-    const { username, password, host, port, dbname } = dados;
-    return {
-        driver: 'postgres',
-        url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
-    };
-}
+const conexao = (dados: Conn): Readonly<{ driver: string; url: string }> => {
+  const { username, password, host, port, dbname } = dados;
+  return {
+    driver: "postgres",
+    url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
+  };
+};
